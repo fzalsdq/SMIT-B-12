2170b144476fab143a823e933a3fe784bcee785f
M. Faisal Siddiqi <sdq7519@gmail.com>
2024-10-08 15:51:44 +0500

    Add files via upload
    This is JavaScript Assignment-1

---
+++ Assignment-1.js
@@ -0,0 +1,263 @@
+
+// //  Assignment-1 
+
+// ********* Chapter-1 *********** 
+
+// // ********* Q1 ***********
+// alert("Hello and Greetings !");
+
+// // ********* Q2 ***********
+// alert("Error! Please Enter a valid password");
+
+// // ********* Q3 ***********
+// alert("Welcome to JS land \n Happy Coding!" );
+
+// // ********* Q4 ***********
+// alert("Welcome to JS land");
+// alert("Happy Coding!" );
+
+// // ********* Q5 ***********
+// alert("Hello... I can run JS through my browser's console" );
+
+// ********* Chapter-2 *********** 
+
+// // ********* Q1 ***********
+// var username;
+
+// // ********* Q2 ***********
+// var myName="Muhammad Faisal Siddiqi";
+
+// // ********* Q3 ***********
+// var message;
+// message="Hello World";
+// alert(message);
+
+// // ********* Q4 ***********
+// var stdtName="John Doe";
+// var age="15 years old";
+// var courseTitle="Certified Mobile Application Development";
+
+// // ********* Q5 ***********
+// var prodOrder="PIZZA";
+// alert(prodOrder + "\n" +prodOrder.substring(0,4)+ "\n" +prodOrder.substring(0,3)+"\n" +prodOrder.substring(0,2)+"\n" +prodOrder.substring(0,1));
+// // prodOrder="PIZZ";
+// // alert(prodOrder);
+// // prodOrder="PIZ";
+// // alert(prodOrder);
+// // prodOrder="PI";
+// // alert(prodOrder);
+// // prodOrder="P";
+// // alert(prodOrder);
+
+// // ********* Q6 ***********
+// var email;
+// email="sdq7519@gmail.com";
+// alert("My email address is " + email);
+
+// // ********* Q7 ***********
+// var book;
+// book="A smarter way to learn Javascript"
+// alert("I am trying to learn from the Book " + book.substring(0,10)+"\n"+book.substring(10,33) );
+
+// // ********* Q8 ***********
+// var message="YAH I can write HTML content through Javascript";
+// document.write(message);
+
+
+
+// // ********* Chapter-3 *********
+
+// // ********* Q1 ***********
+// var age;
+// age=50;
+// alert("I am " + age + " years old");
+
+// // ********* Q2 ***********
+// var visit=20;
+// alert("You have visited this site " + visit + " times");
+
+// // ********* Q3 ***********
+// var birthYear="1975";
+// document.write("My birth year is " + birthYear);
+// document.write("Data type of my declared variable is number");
+
+// // ********* Q4 ***********
+// var custName="John Doe";
+// var prodTitle="T-shirt";
+// var prodQty="10";
+
+// document.write(custName + " ordered " + prodQty + prodTitle + "(s) on XYZ Clothing Store");
+
+// ********* Chapter-4 *********
+// ********* Q1 ***********
+var a,b,c;
+
+// ********* Q2 ***********
+var 123; var 5q1; var ~fe; var (4f); var/hg; var !trt;
+
+var y; var ty4 ; var $rs; var p$3; var _right;
+
+// ********* Q3 ***********
+// ********* Q3a ***********
+document.write(<h1>"Rules for naming JS variables"</h1>);
+// ********* Q3b ***********
+document.write("Variables can only contain $, _ , Alphanumeric and numbers");
+document.write("For Example:"+ <b> +"$my1stVariable"</b>);
+// ********* Q3c ***********
+document.write("Variables must begin with a $, _ or Alphabet")
+document.write("For Example:" + <b> +"$name, _name, or name"</b>);
+// ********* Q3d ***********
+document.write("Variable names are case sensitive");
+
+// ********* Q3e ***********
+document.write("Variable names should not be JS keywords");
+
+
+// ********* Chapter-5 *********
+
+// // ********* Q1 ***********
+// var num1=3;
+// var num2=5;
+// var sum=num1+num2;
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+// // ********* Q2-a ***********
+// var num1=3;
+// var num2=5;
+// var diff=num1-num2;
+// document.write("<br>"+"Difference of " + num1 + " and " + num2 + " is " + diff);
+
+// // ********* Q2-b ***********
+// var num1=3;
+// var num2=5;
+// var prod=num1*num2;
+// document.write("<br>"+"Product of " + num1 + " and " + num2 + " is " + prod);
+
+// // ********* Q2-c ***********
+// var num1=3;
+// var num2=5;
+// var div=num1/num2;
+// document.write("<br>"+"Division of " + num1 + " and " + num2 + " is " + div);
+
+// // ********* Q2-d ***********
+// var num1=3;
+// var num2=5;
+// var mod=num1%num2;
+// document.write("<br>"+"Modulus of " + num1 + " and " + num2 + " is " + mod);
+
+// // ********* Q3 ***********
+// var num;
+// document.write("Value after declaration is: " + num);
+// num=12
+// document.write("<br>" + "Initial Value: " + num);
+// ++num;
+// document.write("<br>" + "Value after increment is: " + num);
+// num = num+7;
+// document.write("<br>" + "Value after addition is: " + num);
+// --num;
+// document.write("<br>" + "Value after decrement is: " + num);
+// num=num%3;
+// document.write("<br>" + "Output: " + " The Remainder is " + num);
+
+// // ********* Q4 ***********
+// var ticketPrice=600;
+// var qty=5
+// var cost = qty * ticketPrice;
+// document.write("<br>" + "Cost of buying " + qty + " tickets is " + cost +"PKR");
+
+// // ********* Q5 ***********
+// var numT=15;
+// var n=1;
+// var prod=numT * n;
+
+// document.write("<br>"+ "Table of " + numT  );
+
+// document.write("<br>"+ numT + " x " + n + " = " + prod );
+// ++n;
+// document.write("<br>"+ numT + " x " + n + " = " + prod*n );
+// ++n;
+// document.write("<br>"+ numT + " x " + n + " = " + prod*n );
+// ++n;
+// document.write("<br>"+ numT + " x " + n + " = " + prod*n );
+// ++n;
+// document.write("<br>"+ numT + " x " + n + " = " + prod*n );
+// ++n;
+// document.write("<br>"+ numT + " x " + n + " = " + prod*n );
+// ++n;
+// document.write("<br>"+ numT + " x " + n + " = " + prod*n );
+// ++n;
+// document.write("<br>"+ numT + " x " + n + " = " + prod*n );
+// ++n;
+// document.write("<br>"+ numT + " x " + n + " = " + prod*n );
+// ++n;
+// document.write("<br>"+ numT + " x " + n + " = " + prod*n );
+// ++n;
+
+
+// ********* Q6 ***********
+var cent=25;
+
+var ctof=(cent*(9/5))+32
+document.write(cent + "oC is " + ctof + "oF");
+
+var farh=70;
+var ftoc=(farh-32)*5/9
+document.write("<br>" + farh + "oF is " + ftoc + "oC");
+
+// ********* Q7 ***********
+var p1=650;
+var q1=3;
+var p2=100;
+var q2=7;
+var sch=100;
+var cost =p1*q1 + p2*q2 + sch ;
+
+document.write("<br>" + "Total Cost of your order is " + cost);
+
+// ********* Q8 ***********
+
+var tMarks=980;
+var mObt=804;
+var per=mObt/tMarks * 100;
+
+document.write("<br>" + "Percentage= "+ per);
+
+// ********* Q9 ***********
+var usCur=10;
+var dtop= usCur * 104.80
+// var pkr= dtop;
+// var ry=25;
+var sCur=25;
+var rtop=sCur*28;
+var tot = dtop + rtop;
+document.write("<br>" + dtop);
+document.write("<br>" + rtop);
+document.write("<br>" + tot);
+document.write("<br>" + "Total Currency in PKR : " + tot );
+
+// ********* Q10 ***********
+var s=3;
+var ans=(s+5)*(10/2);
+// s=s+5;
+// s= s*10;
+// s=s/2;
+document.write(ans);
+
+// ********* Q11 ***********
+var cyear=2024;
+var byear=1984;
+var age =cyear-byear;
+document.write("<br>" + "Your age is " + age);
+
+
+// ********* Q12 ***********
+var r1=20;
+var cf=2 * 3.142 * r1;
+var area=3.142 * (r1*r1);
+
+document.write("<br>"+"Radius of a circle is " + r1);
+
+document.write("<br>"+"Circumference is " + cf);
+
+document.write("<br>"+"The area is " + area);
+
